87ced66584cd5cb65c78f2aed9533b71b27f6829
Jakub <kubaroks@onet.eu>
2022-01-24 17:44:53 +0000

    Initial commit

---
+++ src/main/java/com/example/sheltermanager/ShelterManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.sheltermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelterManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShelterManagerApplication.class, args);
+    }
+
+}
